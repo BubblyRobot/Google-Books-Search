1abe378c3f4fe91710316d8b8052e4ad3c59cf47
BubblyRobot <susyjustice@gmail.com>
2020-10-06 12:14:47 -0400

    added database stuff

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+const DB_USER = "admin";
+const DB_PASSWORD = "7HHNdm94GGBGnqDK";
+const MONGODB = {
+  MONGODB_URI:
+    "mongodb+srv://admin:7HHNdm94GGBGnqDK@cluster0.irwke.mongodb.net/googlebooks?retryWrites=true&w=majority",
+};
+
+const KEYS = {
+  ...MONGODB,
+};
+
+module.exports = KEYS;
--- models/book.js
+++ models/book.js
@@ -5,9 +5,9 @@ const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
   synopsis: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
 });
 
-const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model("googlebooks", bookSchema);
 
 module.exports = Book;
